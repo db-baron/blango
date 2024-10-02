97ab66a403a0b92012a40b5472792f387da26024
Dan Baron <danbenbar@gmail.com>
2024-10-02 22:43:20 +0000

    Finish custom filters

--- blog/models.py
+++ blog/models.py
@@ -17,7 +17,7 @@ class Comment(models.Model):
     content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
     object_id = models.PositiveIntegerField()
     content_object = GenericForeignKey("content_type", "object_id")
-      = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
     modified_at = models.DateTimeField(auto_now=True)
 
 class Post(models.Model):
