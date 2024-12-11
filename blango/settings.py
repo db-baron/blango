24ffc00dca6237e7b572993484f2555a6a62012f
Dan Baron <danbenbar@gmail.com>
2024-12-11 08:01:15 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 from configurations import Configuration
 from configurations import values
+from datetime import timedelta
 import dj_database_url
 # import logging.config
 
@@ -231,6 +232,12 @@ class Dev(Configuration):
             "django_filters.rest_framework.DjangoFilterBackend",
             "rest_framework.filters.OrderingFilter"
         ],
+          "DEFAULT_AUTHENTICATION_CLASSES": [
+            "rest_framework.authentication.BasicAuthentication",
+            "rest_framework.authentication.SessionAuthentication",
+            "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication"
+        ],
     }
 
     SWAGGER_SETTINGS = {
@@ -238,6 +245,11 @@ class Dev(Configuration):
             "Token": {"type": "apiKey", "name": "Authorization", "in": "header"},
             "Basic": {"type": "basic"},
         }
+    }
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
     }
 
 class Prod(Dev):
